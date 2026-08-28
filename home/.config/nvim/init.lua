fd55a535b8fec2b3c4cb9cea77708f829b9e6561
Arham Jain <86415475+arhamj@users.noreply.github.com>
2026-08-28 13:30:36 +0530

    Refresh development environment configuration

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -678,7 +678,6 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -688,6 +687,9 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
+
+        basedpyright = {},
+        ruff = {},
 
         lua_ls = {
           -- cmd = { ... },
